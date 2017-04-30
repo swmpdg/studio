61c644d2df1963ad1e67dd89060f3c3ba072532a
JanUnld <jan@unnold.eu>
2017-04-30 11:23:13 +0200

    Adjusted loaders
    Removed redundant attributes from loader vectors
    Added context property for loading indication

--- src/app/services/context/context.service.ts
+++ src/app/services/context/context.service.ts
@@ -5,11 +5,13 @@ export class Context {
   public name: string;
   public crumbs: Array<string>;
   public frameClass: string;
+  public isLoading: boolean;
 
   public constructor() {
     // Default context values...
     this.name = "Studio";
     this.crumbs = [ "Workspace" ];
     this.frameClass = "frame-white";
+    this.isLoading = true;
   }
 }
