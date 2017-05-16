da5bc23ea5be794b1aa9e442bdabdd213f6cb1e4
JanUnld <jan@unnold.eu>
2017-05-16 22:09:14 +0200

    Locale service improvements
    Excluded the l10n pipe logic to a locale service operation
    Added proper editor folding marker comments

--- src/app/pipes/l10n.pipe.ts
+++ src/app/pipes/l10n.pipe.ts
@@ -7,35 +7,6 @@ export class LocalizationPipe implements PipeTransform {
   public constructor(protected locale: Locale) { }
 
   public transform(resourceKey: string, ...args: any[]): any {
-    const keyParts = resourceKey.split(".");
-    let resourceValue: string | boolean;
-
-    if (keyParts && keyParts.length && this.locale.resources) {
-      let nestedResource = this.locale.resources;
-      for (const keyPart of keyParts) {
-        if (nestedResource.hasOwnProperty(keyPart)) {
-          nestedResource = nestedResource[keyPart];
-        } else { break; }
-      }
-      // Write the nested resource value...
-      resourceValue = nestedResource === this.locale.resources
-        ? nestedResource[resourceKey]
-        : nestedResource;
-    } else {
-      resourceValue = this.locale.resources
-        ? this.locale.resources[resourceKey]
-        : false;
-    }
-
-    if (args.length && resourceValue) {
-      // Are there any arguments...?
-      resourceValue = resourceValue.toString().replace(/{([0-9a-z_$]+)}/gi, (fullMatch, key) => {
-        return args.length === 1 && typeof args[0] === "object"
-          ? args[0][key] // Referring to the inner object instance for named format arguments...
-          : args[key];   // Referring to the argument array index...
-      });
-    }
-    // Finally return the resource value. Or the resource key for no content...
-    return resourceValue || resourceKey;
+    return this.locale.get(resourceKey, ...args);
   }
 }
--- src/app/services/locale/locale.service.ts
+++ src/app/services/locale/locale.service.ts
@@ -15,6 +15,8 @@ export class Locale<T = any> {
 
   public constructor(protected http: Http) { }
 
+  // Properties <editor-fold>
+
   /** Gets the currently used locale */
   public get current() {
     return this._locale || document.body.lang;
@@ -29,6 +31,61 @@ export class Locale<T = any> {
   public get resources() {
     return this._resources;
   }
+
+  // </editor-fold>
+
+  // Traditional operations <editor-fold>
+
+  /**
+   * Gets a value by its given resource key. The resource key can use dot notation to scope into nested resources
+   * @param {string} resourceKey The resource key pointing to the desired value
+   * @param {Array}  args        Optional arguments to interpolate into the resource value
+   * @returns {*} The value for the given resource key
+   */
+  public get(resourceKey: string, ...args: any[]): any {
+    const keyParts = resourceKey.split(".");
+    let resourceValue: string | boolean;
+
+    if (keyParts && keyParts.length && this.resources) {
+      let nestedResource = this.resources;
+      for (const keyPart of keyParts) {
+        if (nestedResource.hasOwnProperty(keyPart)) {
+          nestedResource = nestedResource[keyPart];
+        } else { break; }
+      }
+      // Write the nested resource value...
+      resourceValue = nestedResource === this.resources
+        ? nestedResource[resourceKey]
+        : nestedResource;
+    } else {
+      resourceValue = this.resources
+        ? this.resources[resourceKey]
+        : false;
+    }
+
+    if (args.length && resourceValue) {
+      // Are there any arguments...?
+      resourceValue = resourceValue.toString().replace(/{([0-9a-z_$]+)}/gi, (fullMatch, key) => {
+        return args.length === 1 && typeof args[0] === "object"
+          ? args[0][key] // Referring to the inner object instance for named format arguments...
+          : args[key];   // Referring to the argument array index...
+      });
+    }
+    // Finally return the resource value. Or the resource key for no content...
+    return resourceValue || resourceKey;
+  }
+
+  /**
+   * Returns whether a given resource is currently available by checking its resource url
+   * @param {string} resourceUrl The resource url to check
+   */
+  public has(resourceUrl: string): boolean {
+    return this._resourceKeyCache.has(resourceUrl || "");
+  }
+
+  // </editor-fold>
+
+  // Observable operations <editor-fold>
 
   /**
    * Creates an observable stream holding the desired resource
@@ -142,11 +199,5 @@ export class Locale<T = any> {
     });
   }
 
-  /**
-   * Returns whether a given resource is currently available by checking its resource url
-   * @param {string} resourceUrl The resource url to check
-   */
-  public has(resourceUrl: string): boolean {
-    return this._resourceKeyCache.has(resourceUrl || "");
-  }
+  // </editor-fold>
 }
