ad11a4787f4bd42545f0a560cfd1b5b6740fb1c3
JanUnld <jan@unnold.eu>
2017-05-16 21:31:39 +0200

    Removed forgotten console log

--- src/app/components/color-palette/color-palette.component.ts
+++ src/app/components/color-palette/color-palette.component.ts
@@ -7,15 +7,11 @@ import { Color } from "../../core/color";
   styleUrls: [ "./color-palette.component.scss" ]
 })
 export class ColorPaletteComponent {
+  @Input() activeColor: Color = Color.parse("#1a1a1a");
   @Input() collections: Array<Color> = [
     Color.parse("#2B2238"),
     Color.parse("#7CBABC"),
     Color.parse("#66CC7C"),
     Color.parse("#CC6666")
   ];
-  @Input() activeColor: Color = Color.parse("#1a1a1a");
-
-  public constructor() {
-    console.log(this.collections);
-  }
 }
