b3fe2b6265e32d4a016fc0ac4452b067ef0b6f77
JanUnld <jan@unnold.eu>
2017-05-25 18:57:28 +0200

    Implemented #5 navigation group concept
    Added related scss styles
    Adjusted navigation componente template
    Adjusted navigation item interface

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -10,20 +10,50 @@ import { BaseComponent } from "app/core/base-component";
 export class NavigationComponent extends BaseComponent {
   public itemBaseClass = "mdi";
   public items: NavigationItem[] = [{
-    itemClass: "mdi-brush",
-    label: "Workspace"
+    type: "group",
+    label: "Studio",
+    links: [{
+      type: "link",
+      label: "Workspace",
+      itemClass: "mdi-brush"
+    }, {
+      type: "link",
+      label: "Storage",
+      itemClass: "mdi-floppy"
+    }, {
+      type: "link",
+      label: "Settings",
+      itemClass: "mdi-settings"
+    }]
   }, {
-    itemClass: "mdi-floppy",
-    label: "Storage"
-  }, {
-    itemClass: "mdi-bookmark-outline",
-    label: "Help"
+    type: "group",
+    label: "General",
+    links: [{
+      type: "link",
+      label: "Github",
+      itemClass: "mdi-github-circle"
+    }, {
+      type: "link",
+      label: "Help"
+    }]
   }];
 }
 
+/** Provides the available signature for navigation items */
 export interface NavigationItem {
+  /**
+   * Gets or sets the item type for this item. There are two types available:
+   *  1. group: Requires the links property to be defined
+   *  2. link:  Requires the link_url and itemClass property to be defined
+   */
+  type: "group" | "link";
+  /** Gets or sets the label for this item */
   label: string;
 
+  /** Gets or sets the link url for this item */
+  link_url?: string;
+  /** Gets or sets the link children for this item */
+  links?: NavigationItem[];
+  /** Gets or sets the item class for this item */
   itemClass?: string;
-  link?: string;
 }
