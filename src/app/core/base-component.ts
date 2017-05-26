6d359cfbf1d49e488a9dc51940f95925ceb66f9e
JanUnld <jan@unnold.eu>
2017-05-26 20:25:19 +0200

    Added now timestamp to base component features

--- src/app/core/base-component.ts
+++ src/app/core/base-component.ts
@@ -1,5 +1,8 @@
 /** Provides abstract base features for component inheritance */
 export abstract class BaseComponent {
+  protected get now(): Date {
+    return new Date();
+  }
   /** Placeholder getter for an empty string value */
   protected get empty(): string {
     return "";
