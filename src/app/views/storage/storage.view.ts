4c33898a68d4a772b6c47d421febaf84a549550b
JanUnld <jan@unnold.eu>
2017-05-28 22:40:49 +0200

    Added experimental storage file table concept

--- src/app/views/storage/storage.view.ts
+++ src/app/views/storage/storage.view.ts
@@ -1,16 +1,23 @@
 import { Component, OnDestroy } from "@angular/core";
 
 import { Storage } from "app/services/storage/storage.service";
-import { Locale } from "app/services/locale/locale.service";
 import { Context } from "../../services/context/context.service";
 
 import { BaseComponent } from "app/core/base-component";
 
 @Component({
   selector: "storage-view",
-  templateUrl: "./storage.view.html"
+  templateUrl: "./storage.view.html",
+  styleUrls: [ "./storage.view.scss" ]
 })
 export class StorageViewComponent extends BaseComponent implements OnDestroy {
+  public files: any[] = [
+    { name: "vandalsquad_studio_intro", ext: "vc", last_mod: this.now },
+    { name: "piece_01", ext: "vc", last_mod: this.now },
+    { name: "piece_02", ext: "vc", last_mod: this.now },
+    { name: "some_juicy_experiments", ext: "vc", last_mod: this.now }
+  ];
+
   public constructor(
     protected storage: Storage,
     protected context: Context) {
