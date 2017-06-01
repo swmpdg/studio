e85c2946f8903a9c747c6010443e05402e8085a3
JanUnld <jan@unnold.eu>
2017-06-01 23:21:10 +0200

    Improved storage view

--- src/app/views/storage/storage.view.ts
+++ src/app/views/storage/storage.view.ts
@@ -12,10 +12,10 @@ import { BaseComponent } from "app/core/base-component";
 })
 export class StorageViewComponent extends BaseComponent implements OnDestroy {
   public files: any[] = [
-    { name: "vandalsquad_studio_intro", ext: "io", last_mod: this.now },
-    { name: "piece_01", ext: "io", last_mod: this.now },
-    { name: "piece_02", ext: "io", last_mod: this.now },
-    { name: "some_juicy_experiments", ext: "io", last_mod: this.now }
+    { name: "unique pieces", last_mod: this.now, icon: "mdi mdi-folder-outline icon-folder" },
+    { name: "vandalsquad_studio_intro.vsc", last_mod: this.now, icon: "mdi mdi-note-outline" },
+    { name: "piece_01.vsc", last_mod: this.now, icon: "mdi mdi-note-outline" },
+    { name: "piece_02.vsc", last_mod: this.now, icon: "mdi mdi-note-outline" }
   ];
 
   public constructor(
